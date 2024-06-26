b00b1cc2b5f22a472c16c57996272f13bc16ec14
BittingBarracuda <kmartinez2397@gmail.com>
2024-06-26 11:39:52 +0200

    Implemented a way to create zip files from numpy arrays.

--- resizer.py
+++ resizer.py
@@ -7,6 +7,8 @@ import random
 import cv2
 import os
 
+TEXT_1 = 'Operation in process. Please wait.'
+
 def get_datetime(display=False):
     if display:
         return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -18,6 +20,31 @@ def generate_random_string(k=16):
 def delete_file(file_name):
     os.remove(os.path.join('files', file_name))
 
+def save_np_images(converted_imgs, file_names):
+    aux_file_names = []
+    for converted_img, file_name in zip(converted_imgs, file_names):
+        aux_file_name = f'{generate_random_string()}_{get_datetime()}.npy'
+        np.save(os.path.join('files', aux_file_name), converted_img)
+        aux_file_names.append(aux_file_name)
+    return [(x, y) for x, y in zip(file_names, aux_file_names)]
+
+def merge_np_images(np_files, progress_bar):
+    zipfile_name = f'{generate_random_string()}_{get_datetime()}.zip'
+    prog_per_file = 0.1 / len(np_files)
+    with zipfile.ZipFile(os.path.join('files', zipfile_name), 'w') as file:
+        i = 1
+        for file_name, np_file in np_files:
+            arr = np.load(os.path.join('files', np_file))
+            file_ext = file_name[file_name.rindex('.'):]
+            _, buf = cv2.imencode(file_ext, arr)
+            file.writestr(file_name, buf)
+
+            progress_bar.progress(0.9 + (i * prog_per_file), TEXT_1)
+            i += 1
+    for _, np_file in np_files:
+        delete_file(np_file)
+    return zipfile_name
+
 def create_zip_file(converted_imgs, file_names):
     zipfile_name = f'{generate_random_string()}_{get_datetime()}.zip'
     with zipfile.ZipFile(os.path.join('files', zipfile_name), 'w') as file:
@@ -27,18 +54,20 @@ def create_zip_file(converted_imgs, file_names):
             file.writestr(file_name, buf)
     return zipfile_name
 
-def merge_zip_files(zip_files_names):
+def merge_zip_files(zip_files_names, progress_bar):
     print(f'[! {get_datetime(True)}] Merging {zip_files_names}')
+    prog_per_file = 0.1 / len(zip_files_names)
+
     with zipfile.ZipFile(os.path.join('files', zip_files_names[0]), 'a') as file:
+        i = 2
         for zip_name in zip_files_names[1:]:
             with zipfile.ZipFile(os.path.join('files', zip_name), 'r') as zip_file:
-                # print(f'[{get_datetime(True)}] Processing {zip_name}')
                 for name in zip_file.namelist():
-                    # print(f'[{get_datetime(True)}] Processing {name}')
                     file.writestr(name, zip_file.open(name).read())
+            progress_bar.progress(0.9 + (i * prog_per_file), TEXT_1)
+            i += 1
     
     for zip_file_name in zip_files_names[1:]:
-        print(f'[! {get_datetime()}] Deleting files...')
         delete_file(zip_file_name)
     
     return zip_files_names[0]
@@ -46,9 +75,9 @@ def merge_zip_files(zip_files_names):
 def resize_image(images_data, progress_bar, multiplier=None, size_x=None, size_y=None):
     converted_imgs, file_names = [], []
     zip_files = []
-    progress_per_img = 1.0 / len(images_data)
+    progress_per_img = 0.9 / len(images_data)
     for i, image_data in enumerate(images_data):
-        progress_bar.progress((i + 1) * progress_per_img, 'Operation in process. Please wait.')
+        progress_bar.progress((i + 1) * progress_per_img, TEXT_1)
         jpg_as_np = np.fromstring(image_data.getvalue(), dtype=np.uint8)
         img = cv2.imdecode(jpg_as_np, cv2.IMREAD_COLOR)
         file_names.append(image_data.name)
@@ -71,8 +100,42 @@ def resize_image(images_data, progress_bar, multiplier=None, size_x=None, size_y
             zip_files.append(create_zip_file(converted_imgs, file_names))
             converted_imgs, file_names = [], []
     
-    progress_bar.progress(1.0, 'Process completed!')
     if converted_imgs != []:
         zip_files.append(create_zip_file(converted_imgs, file_names))
 
-    return merge_zip_files(zip_files)
+    return merge_zip_files(zip_files, progress_bar)
+
+
+def resize_image_np(images_data, progress_bar, multiplier=None, size_x=None, size_y=None):
+    converted_imgs, file_names = [], []
+    all_files_names = []
+    progress_per_img = 0.9 / len(images_data)
+
+    for i, image_data in enumerate(images_data):
+        progress_bar.progress((i + 1) * progress_per_img, TEXT_1)
+        jpg_as_np = np.fromstring(image_data.getvalue(), dtype=np.uint8)
+        img = cv2.imdecode(jpg_as_np, cv2.IMREAD_COLOR)
+        file_names.append(image_data.name)
+        # print(f'[! {get_datetime(True)}] Resizing {file_names[-1]}!')
+
+        if multiplier != None:
+            new_img = cv2.resize(img, 
+                                dsize=None,
+                                fx=multiplier, 
+                                fy=multiplier,
+                                interpolation=cv2.INTER_LANCZOS4)
+        else:
+            new_img = cv2.resize(img,
+                                dsize=(size_x, size_y),
+                                interpolation=cv2.INTER_LANCZOS4)
+        
+        converted_imgs.append(new_img)
+        if (i+1) % 50 == 0:
+            print(f'[! {get_datetime(True)}] {i+1} images read, creating numpy file...')
+            all_files_names.extend(save_np_images(converted_imgs, file_names))
+            converted_imgs, file_names = [], []
+    
+    if converted_imgs != []:
+        all_files_names.extend(save_np_images(converted_imgs, file_names))
+
+    return merge_np_images(all_files_names, progress_bar)
--- streamlit_site.py
+++ streamlit_site.py
@@ -1,27 +1,36 @@
 import streamlit as st
-from resizer import resize_image, delete_file, get_datetime, generate_random_string
+from resizer import resize_image, resize_image_np, delete_file, get_datetime
 import os
 
-MAX_FREE_FILES = 10_000
+MAX_FREE_FILES = 1_000
 FILE_UPLOADER_KEY = 'file_uploader_key'
+MULTIPLIER_SEL = 'mult_selected'
 
 def delete_file_st(zipfile_name):
     print(f'[! {get_datetime(True)}] Downloading {zipfile_name} --- Contains {len(file_uploader)} files!')
     delete_file(zipfile_name)
-
+   
 if FILE_UPLOADER_KEY not in st.session_state:
     st.session_state[FILE_UPLOADER_KEY] = 0
+if MULTIPLIER_SEL not in st.session_state:
+    st.session_state[MULTIPLIER_SEL] = False
 
 file_uploader = st.file_uploader('Choose your files', 
                                  accept_multiple_files=True,
                                  key=st.session_state[FILE_UPLOADER_KEY])
+
+st.markdown('#')   
+select_box = st.selectbox('Resize by', 
+                          options=['Multiplier', 'Horizontal and vertical sizes'])
+st.session_state[MULTIPLIER_SEL] = (select_box != 'Multiplier')
 
 st.markdown('#')
 dimension_slider = st.slider('Size multiplier (keeps aspect ratio)',
                             min_value=0.1,
                             max_value=10.0,
                             key='dimension_slider',
-                            step=0.1)
+                            step=0.1,
+                            disabled=st.session_state[MULTIPLIER_SEL])
 
 st.markdown('#')
 c1, c2 = st.columns(2)
@@ -29,24 +38,14 @@ with c1:
     x_dim = st.number_input('Horizontal pixels', 
                             key='x_dim',
                             min_value=1,
-                            step=1)
+                            step=1,
+                            disabled=(not st.session_state[MULTIPLIER_SEL]))
 with c2:
     y_dim = st.number_input('Vertical pixels',
                             key='y_dim',
                             min_value=1,
-                            step=1)
-
-st.markdown('#')   
-select_box = st.selectbox('Resize by', options=['Multiplier', 'Horizontal and vertical sizes'])
-# c1, c2 = st.columns(2)
-# with c1:
-#     dimension_checkbox = st.checkbox('Resize by multiplier',
-#                                      value=True,
-#                                      key='dimension_checkbox')
-# with c2:
-#     sizes_checkbox = st.checkbox('Resize by horizontal and vertical dimensions',
-#                                  value=False,
-#                                  key='sizes_checkbox')
+                            step=1,
+                            disabled=(not st.session_state[MULTIPLIER_SEL]))
 
 st.markdown('#')   
 c1, c2 = st.columns(2)
@@ -57,12 +56,8 @@ with c2:
     start_conversion = st.button('Start conversion', 
                                  key='start_conversion')
 
-# if sizes_checkbox:
-#     dimension_checkbox = False 
-# if dimension_checkbox:
-#     sizes_checkbox = False
-
 if start_conversion and (file_uploader is not None):
+    st.markdown('#')
     progress_bar = st.progress(0, 'Operation in process. Please wait.')
     n = len(file_uploader)
     if n < MAX_FREE_FILES:
@@ -70,6 +65,8 @@ if start_conversion and (file_uploader is not None):
             zipfile_name = resize_image(file_uploader, progress_bar, multiplier=dimension_slider)
         else:
             zipfile_name = resize_image(file_uploader, progress_bar, size_x=x_dim, size_y=y_dim)
+        
+        progress_bar.progress(1.0, 'Process completed!')
         with open(os.path.join('files', zipfile_name), 'rb') as file:
             download_button = st.download_button('Download data!', 
                                                  key='download_button',
@@ -83,5 +80,3 @@ if start_conversion and (file_uploader is not None):
 if delete_button:
     st.session_state[FILE_UPLOADER_KEY] += 1
     st.rerun()
-        
-
